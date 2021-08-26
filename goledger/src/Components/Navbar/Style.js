f386aed042def50326cf62e5788349997cabf709
nYCSTs <lucascandradex14@gmail.com>
2021-08-26 20:51:14 -0300

    Style adjusts on navbar

--- goledger/src/Components/Navbar/Style.js
+++ goledger/src/Components/Navbar/Style.js
@@ -2,21 +2,12 @@ import styled from 'styled-components';
 
 export const Nav = styled.div`
     display: flex;
-    height: 56px;
     background-color: #04AA6D;
-    align-items: flex-end;
     border-bottom-left-radius: 12px;
     border-bottom-right-radius: 12px;
-    margin-bottom: 24px;
-`;
-
-export const NavContent = styled.div`
-    display: flex;
     justify-content: space-between;
-    width: 100%;
-    height: 45px;
-    margin: 0 8px;
-
+    height: 68px;
+    padding: 12px;
 `;
 
 export const Button = styled.button`
@@ -24,56 +15,30 @@ export const Button = styled.button`
     align-items: center;
     background-color: Transparent;
     border: none;
-    padding: 0;
-    
+    padding: 6px;
 `;
 
 export const NavItem = styled.div`
-    margin-right: 14px;
-    width: 92px;
-    align-items: center;
-`;
-
-export const P = styled.p`
-    margin: 0;
-`;
-
-export const Dropdown = styled.div`
     display: flex;
+    width: 92px;
     justify-content: space-around;
     border: 1px solid black;
     border-radius: 8px;
-    padding: 6px;
 `;
 
 export const A = styled.a`
     text-decoration: none;
-    color: black;
 `;
 
 export const Links = styled.div`
     display: flex;
-`;
-
-export const DropdownDiv = styled.div`
-    display: flex;
-    flex-direction: column;
-    background-color: white;
-    border-radius: 12px;
-    border: 1px solid black;
-    margin: 0;
-    width: min-content;
-    white-space: nowrap;
-    padding: 6px 0;
+    align-items: flex-end;
+    width: 200px;
+    justify-content: space-between;
 `;
 
 export const HomePageLink = styled(A)`
-    font-size: 28px;
+    font-size: 36px;
     font-weight: bold;
-`;
-
-export const DropdownItem = styled(A)`
-    padding: 6px;
     color: black;
-    font-size: 14px;
 `;
--- goledger/src/Components/Navbar/index.js
+++ goledger/src/Components/Navbar/index.js
@@ -1,78 +1,20 @@
-import { useState } from 'react';
-import { IoIosArrowDropdown } from 'react-icons/io';
 import {
-  Nav, NavContent, Button, Dropdown, A, NavItem,
-  DropdownDiv, DropdownItem, Links, HomePageLink,
+  Nav, A, NavItem,
+  Links, HomePageLink, Button,
 } from './Style';
 
-const Navbar = () => {
-  const [artistDropdownState, setArtistDropdownState] = useState(false);
-  const [albumDropdownState, setAlbumDropdownState] = useState(false);
-  const [valueType, setValueType] = useState('');
-
-  const resetDropdown = () => {
-    setArtistDropdownState(false);
-    setAlbumDropdownState(false);
-  };
-
-  const showDropdown = () => (
-    <DropdownDiv>
-      <DropdownItem onClick={() => resetDropdown()} href={`/${valueType}/register/`}>
-        Cadastrar
-        {' '}
-        {valueType}
-      </DropdownItem>
-      <DropdownItem onClick={() => resetDropdown()} href="/">
-        Atualizar
-        {' '}
-        {valueType}
-      </DropdownItem>
-      <DropdownItem onClick={() => resetDropdown()} href="/">
-        Deletar
-        {' '}
-        {valueType}
-      </DropdownItem>
-    </DropdownDiv>
-  );
-
-  return (
-    <Nav>
-      <NavContent>
-        <HomePageLink href="/">GoLedger</HomePageLink>
-        <Links>
-          <NavItem>
-            <Dropdown>
-              <A href="/artistas/">Artistas</A>
-              <Button onClick={() => {
-                setArtistDropdownState(!artistDropdownState);
-                setAlbumDropdownState(false);
-                setValueType('Artista');
-              }}
-              >
-                <IoIosArrowDropdown />
-              </Button>
-            </Dropdown>
-            {artistDropdownState ? showDropdown() : null}
-          </NavItem>
-          <NavItem>
-            <Dropdown>
-              <A href="/albuns">Albuns</A>
-              <Button onClick={() => {
-                setAlbumDropdownState(!albumDropdownState);
-                setArtistDropdownState(false);
-                setValueType('Album');
-              }}
-              >
-                <IoIosArrowDropdown />
-              </Button>
-            </Dropdown>
-            {albumDropdownState ? showDropdown() : null}
-          </NavItem>
-        </Links>
-      </NavContent>
-
-    </Nav>
-  );
-};
+const Navbar = () => (
+  <Nav>
+    <HomePageLink href="/">GoLedger</HomePageLink>
+    <Links>
+      <NavItem>
+        <A href="/artistas/"><Button>Artistas</Button></A>
+      </NavItem>
+      <NavItem>
+        <A href="/albuns"><Button>Albuns</Button></A>
+      </NavItem>
+    </Links>
+  </Nav>
+);
 
 export default Navbar;
