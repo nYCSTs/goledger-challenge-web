23ff415eb5a9bc91d506fcefe9c3683681da1034
nYCSTs <lucascandradex14@gmail.com>
2021-08-28 16:20:17 -0300

    Alert when artist is created and redirect to artists listing page

--- goledger/src/Pages/RegisterArtist/index.js
+++ goledger/src/Pages/RegisterArtist/index.js
@@ -3,18 +3,24 @@ import RegisterAsset from '../../Components/AssetForm';
 import ArtistForm from '../../Components/ArtistForm';
 import countryCodes from '../../Constants/countryCodes';
 import { registerArtist } from '../../Services/artistServices';
+import { useHistory } from 'react-router';
 
 const RegisterArtist = () => {
+  const history = useHistory();
   const [artistName, setArtistName] = useState();
   const [artistDescription, setArtistDescription] = useState();
   const [selectedCountry, setSelectedCountry] = useState();
 
   const submitRegister = async () => {
-    await registerArtist(
+    const response = await registerArtist(
       artistName,
       artistDescription,
       countryCodes.filter((r) => r.code === selectedCountry)[0].name,
     );
+    if (reseponse.status === 200) {
+      alert("Artist was successfully registered!");
+      history.push('/artists');
+    }
   };
 
   return (
