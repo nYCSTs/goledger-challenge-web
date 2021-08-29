f427d4dc056e2fc69cb2b5778b4becdf4c58a900
nYCSTs <lucascandradex14@gmail.com>
2021-08-29 10:08:02 -0300

    Fix console warning

--- goledger/src/Components/StreamingForm/index.js
+++ goledger/src/Components/StreamingForm/index.js
@@ -5,7 +5,7 @@ import {
 const StreamingForm = ({ streamingServiceName, setStreamingServiceName }) => (
   <>
     <P>Streaming service name:</P>
-    <Input value={streamingServiceName} onChange={(e) => setStreamingServiceName(e.target.value)} />
+    <Input value={streamingServiceName || ''} onChange={(e) => setStreamingServiceName(e.target.value)} />
   </>
 );
 
